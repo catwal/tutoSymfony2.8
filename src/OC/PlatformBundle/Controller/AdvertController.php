d372cecfe7bcd4917a127d7a05f7d90ab51f94ac
catwal <kitekate.cw@gmail.com>
2019-07-25 15:58:24 +0200

    url routing explainations

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -10,10 +10,34 @@ class AdvertController extends Controller
     public function indexAction()
     {
 
-//        return new Response("Hello-World in page index");
-        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
-            // ...
-        ));
+//  return new Response("Hello-World in page index");
+//      $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig');
+//        return new Response($content);
+
+        return $this->render(
+            'OCPlatformBundle:Advert:index.html.twig',
+            [
+                'name' => 'you !',
+            ]
+        );
+    }
+
+    /**
+     * @param $id
+     *
+     * @return Response
+     */
+    public function viewAction($id)
+    {
+        return new Response("print id from url /platform/advert/: " . $id);
+    }
+
+    public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '" . $slug . "', créée en " . $year . " et au format " . $format . "."
+        );
     }
 
 }
