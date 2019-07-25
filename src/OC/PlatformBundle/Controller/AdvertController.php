b14c070b996f5b6ebcb5db25f3e10047b6078c32
catwal <kitekate.cw@gmail.com>
2019-07-25 18:10:00 +0200

    controller handling session

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,6 +3,8 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
@@ -16,9 +18,7 @@ class AdvertController extends Controller
 
         return $this->render(
             'OCPlatformBundle:Advert:index.html.twig',
-            [
-                'name' => 'you !',
-            ]
+            array()
         );
     }
 
@@ -27,25 +27,71 @@ class AdvertController extends Controller
      *
      * @return Response
      */
-//    public function viewAction($id)
-//    {
-//        return new Response("print id from url /platform/advert/: " . $id);
-//    }
-
-
-    public function viewAction()
+    // Request
+    public function viewAction($id, Request $request)
     {
-        $url = $this->get('router')->generate(
-            'oc_platform_view',
-            ['id' => 5],
-//            constraint to absolute url
-            true
-        );
-        // same method shorter
-        $url = $this->generateUrl('oc_platform_view', array('id'=>5));
+        // recovery of word tag in url (by request HTTP by $request) and print it into view
+        // method: query, request, cookies, server, headers, attributes
+        // method was recovery from a clic on a link with method GET or via a POST from form
+        if ($request->isMethod('POST')) {
+            //TODO
+        };
+        $tag = $request->query->get('tag');
+        // construction of response object
 
-        return new Response("url of this template ended by /platform/advert/5: " . $url);
+//        $response = new Response();
+//        $response->setContent("Ceci est une page d'erreur 404");
+//        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+//
+//        return $response;
+
+//        return new Response(
+//            "<html><body> print id from url /platform/advert/: " . $id . "and a tag : " . $tag . "</body></html>"
+//        );
+
+        if(!$tag){
+            // redirection page
+           // $url = $this->get('router')->generate('oc_platform_homepage');
+            //return new RedirectResponse($url);
+            return $this->redirectToRoute('oc_platform_homepage');
+        }
+
+
+        //Manipulation of session
+
+        $session= $request->getSession();
+        $userId = $session->get('user_id');
+        $session->set('user_id', 91);
+
+        $session = $request->getSession();
+        $session->getFlashBag()->add('info', 'bien enregistrée');
+        $session->getFlashBag()->add('info', 'oui, c\'est bien enregistrée');
+        dump($session);
+
+
+        return $this->render(
+            'OCPlatformBundle:Advert:index.html.twig',
+            [
+                'id'  => $id,
+                'tag' => $tag,
+            ]
+        );
     }
+
+
+//    public function viewAction()
+//    {
+//        $url = $this->get('router')->generate(
+//            'oc_platform_view',
+//            ['id' => 5],
+////            constraint to absolute url
+//            true
+//        );
+//        // same method shorter
+//        $url = $this->generateUrl('oc_platform_view', array('id'=>5));
+//
+//        return new Response("url of this template ended by /platform/advert/5: " . $url);
+//    }
 
     public function viewSlugAction($slug, $year, $format)
     {
